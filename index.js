29d60311e1aff56034bd4ce32e2cfa7b5f463534
Avneesh0404 <ajarangal_be22@thapar.edu>
2025-10-19 11:26:45 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -16,13 +16,10 @@ console.log(`Signalling server starting (env: ${process.env.NODE_ENV || 'develop
 console.log(`CORS allowed origin: ${CORS_ORIGIN}`);
 
 io.on("connection", (socket) => {
-  console.log("socket connected", socket.id);
-
   // Join a room
   socket.on("join-room", ({ roomId }) => {
     if (!roomId) return;
     socket.join(roomId);
-    console.log(`${socket.id} joined ${roomId}`);
 
     // notify others in the room that a peer joined
     socket.to(roomId).emit("peer-joined", { peerId: socket.id });
@@ -34,7 +31,6 @@ io.on("connection", (socket) => {
   // Forward offer to other peers in the room
   socket.on("offer", ({ roomId, offer, to }) => {
     if (!roomId || !offer) return;
-    console.log(`${socket.id} -> offer to room ${roomId} ${to ? "to:"+to : ""}`);
     if (to) {
       io.to(to).emit("offer", { offer, from: socket.id });
     } else {
@@ -64,8 +60,6 @@ io.on("connection", (socket) => {
 
   // Clean up on disconnect and notify peers
   socket.on("disconnect", (reason) => {
-    console.log("socket disconnected", socket.id, reason);
-    // socket.rooms contains all rooms this socket belongs to (including a room with socket.id)
     for (const roomId of socket.rooms) {
       if (roomId === socket.id) continue;
       socket.to(roomId).emit("peer-left", { peerId: socket.id });
